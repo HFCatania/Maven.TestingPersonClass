b82b4d94ddf3d8d57f867faa6e071c5736b97b12
Henry <cataniahf@gmail.com>
2019-05-31 13:29:16 -0400

    Finished lab

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,103 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private String hairColor;
+    private String eyeColor;
+    private String homeTown;
+    private boolean drives;
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
+        height = 6;
+        hairColor= "brown";
+        eyeColor = "green";
+        homeTown = "Philadelphia";
+        drives = true;
+
     }
 
+
     public Person(int age) {
+        this.age = age;
+
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, int height, String hairColor, String eyeColor, String homeTown, boolean drives) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.homeTown = homeTown;
+        this.drives = drives;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void setHomeTown(String homeTown) {
+        this.homeTown = homeTown;
+    }
+
+    public void setDrives(boolean drives) {
+        this.drives = drives;
     }
 
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return this.age;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public String getHairColor() {
+        return this.hairColor;
+    }
+
+    public String getEyeColor() {
+        return this.eyeColor;
+    }
+
+    public String getHomeTown() {
+        return this.homeTown;
+    }
+
+    public boolean getDrives() {
+        return this.drives;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -68,6 +68,40 @@ public class TestPerson {
         Assert.assertEquals(expectedName, actualName);
     }
 
+
+    @Test
+    public void testConstructorWithAll() {
+        Person person = new Person();
+        String expectedName = "";
+        int expectedAge = Integer.MAX_VALUE;
+        int expectedHeight = 6;
+        String expectedHairColor= "brown";
+        String expectedEyeColor = "green";
+        String expectedHomeTown = "Philadelphia";
+        boolean expectedDrives = true;
+
+
+        String actualName = person.getName();
+        int actualAge = person.getAge();
+        int actualHeight = person.getHeight();
+        String actualHairColor= person.getHairColor();
+        String actualEyeColor = person.getEyeColor();
+        String actualHomeTown = person.getHomeTown();
+        boolean actualDrives = person.getDrives();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedHairColor, actualHairColor);
+        Assert.assertEquals(expectedEyeColor, actualEyeColor);
+        Assert.assertEquals(expectedHomeTown, actualHomeTown);
+        Assert.assertEquals(expectedDrives, actualDrives);
+
+    }
+
+
+
+
     @Test
     public void testSetName() {
         // Given
@@ -94,5 +128,84 @@ public class TestPerson {
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void setHeight() {
+        Person person = new Person();
+        Integer expected = 2147483647;
+        person.setHeight(expected);
+        Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setHairColor() {
+        Person person = new Person();
+        String expected = "red";
+        person.setHairColor(expected);
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void setEyeColor() {
+        Person person = new Person();
+        String expected = "brown";
+        person.setEyeColor(expected);
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void setHomeTown() {
+        Person person = new Person();
+        String expected = "new york";
+        person.setHomeTown(expected);
+        String actual = person.getHomeTown();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setDrives() {
+        Person person = new Person();
+        Boolean expected = true;
+        person.setDrives(expected);
+        Boolean actual = person.getDrives();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getName() {
+        Person person = new Person();
+
+    }
+
+    @Test
+    public void getAge() {
+    }
+
+    @Test
+    public void getHeight() {
+    }
+
+    @Test
+    public void getHairColor() {
+    }
+
+    @Test
+    public void getEyeColor() {
+    }
+
+    @Test
+    public void getHomeTown() {
+    }
+
+    @Test
+    public void isDrives() {
     }
 }
